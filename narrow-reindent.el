c5cd95bfeaec7910271bb21de9e8c2c91f01b4d1
J David Smith <emallson@archlinux.us>
2015-06-01 19:09:56 -0400

    Added license (GPLv3)

--- narrow-reindent.el
+++ narrow-reindent.el
@@ -1,5 +1,21 @@
 ;;; narrow-reindent.el --- Summary
 ;;;
+;;; Copyright: © 2015 J David Smith
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;;
 ;;; Author: J David Smith <emallson@atlanis.net>
 ;;; Maintainer: J David Smith <emallson@atlanis.net>
 ;;; Homepage: https://github.com/emallson/narrow-reindent.el
